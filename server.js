b9644e24a3c8adb73d69a2172029811882862984
Ryan-L-UK <Ryanlewery@Gmail.com>
2025-12-16 14:12:03 +0000

    Initial-Load

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 6969;
+
+// Serve static folders
+app.use('/Pages', express.static(path.join(__dirname, 'Pages')));
+app.use('/Foundry', express.static(path.join(__dirname, 'Foundry')));
+app.use('/Sources', express.static(path.join(__dirname, 'Sources')));
+
+// Default route → index.html at root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`MonsterFactory running at http://localhost:${port}`);
+});
